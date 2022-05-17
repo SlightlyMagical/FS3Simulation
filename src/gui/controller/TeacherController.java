e40c435435601820fcc41b14e074e1cb55595244
m-eunbi <mariahansen.91@hotmail.com>
2022-05-17 16:04:00 +0200

    Created teacher's view and teachercontroller

---
+++ src/gui/controller/TeacherController.java
@@ -0,0 +1,61 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TeacherController {
+    public TableView tvBank;
+    public TableColumn tcBankFirstName;
+    public TableColumn tcBankLastName;
+    public TableView tvAssigned;
+    public TableColumn tcAssignedFirstName;
+    public TableColumn tcAssignedLastName;
+    public TableView tvAssignedStudent;
+    public TableColumn tcAssignedStudentUser;
+    public TableColumn tcAssignedStudentPassword;
+    public TableView tvStudentView;
+    public TableColumn tcStudentUser;
+    public TableColumn tcStudentPassword;
+    public TextField txtfUsername;
+    public PasswordField txtPassword;
+    public PasswordField txtRepeatpassword;
+
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    public void handleCreate(ActionEvent actionEvent) {
+    }
+
+    public void handleEdit(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleCreateCopy(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignToStudent(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignedEdit(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignedDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignedCopy(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignedStudenAdd(ActionEvent actionEvent) {
+    }
+
+    public void handleAssignedStudentRemove(ActionEvent actionEvent) {
+    }
+
+    public void handleStudentCreateUser(ActionEvent actionEvent) {
+    }
+}
