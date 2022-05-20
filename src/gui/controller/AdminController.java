b8f1047ec9391e924191fd249012fa71eb5bc42a
m-eunbi <mariahansen.91@hotmail.com>
2022-05-20 14:09:50 +0200

    Started AdminController and CreateNewController

---
+++ src/gui/controller/AdminController.java
@@ -0,0 +1,41 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AdminController {
+    public TableView tvAdminUsername;
+    public TableView tvSchoolName;
+    public TableView tvStudentlist;
+    public TableColumn tcStudentUsername;
+    public TableColumn tcStudentPassword;
+    public TableView tvTeacherUsername;
+
+    public void handleAdminCreate(ActionEvent actionEvent) {
+    }
+
+
+    public void handleAdminDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleSchoolCreate(ActionEvent actionEvent) {
+    }
+
+    public void handleSchoolDelte(ActionEvent actionEvent) {
+    }
+
+    public void handleStudentCreate(ActionEvent actionEvent) {
+    }
+
+    public void handleStudentDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleTeacherCreate(ActionEvent actionEvent) {
+    }
+
+    public void handleTeacherDelete(ActionEvent actionEvent) {
+    }
+}
+
---
+++ src/gui/controller/CreateNewController.java
@@ -0,0 +1,19 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateNewController {
+    public TextField txtUsername;
+    public PasswordField txtChoosePassword;
+    public PasswordField txtConfirmPassword;
+    public ComboBox cbUsertypes;
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleCreateNew(ActionEvent actionEvent) {
+    }
+}
